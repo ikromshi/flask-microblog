26336c004a67648bd3250999e9b5af368e0994b7
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-26 00:43:00 -0400

    profile edit page added; fix url for endpoint issue

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -31,3 +31,8 @@ class RegistrationForm(FlaskForm):
     user = User.query.filter_by(username=email.data).first()
     if user is not None:
       raise ValidationError("Please use a different email address")
+
+class EditProfileForm(FlaskForm):
+  username = StringField("Username", validators=[DataRequired()])
+  about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
+  submit = SubmitField("Submit")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask_login import login_user, current_user, logout_user, login_required
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import CreateAccountForm, LoginForm, RegistrationForm
+from app.forms import CreateAccountForm, EditProfileForm, LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app.models import User
@@ -86,3 +86,19 @@ def user(username):
         {"author": user, "body": "Test post 2"},
     ]
     return render_template("user.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_required():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been save")
+        return redirect(url_for("edit_profile"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
