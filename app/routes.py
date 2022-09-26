1a730b2a4d9cab7a2add638ece6b20f5818c9732
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-26 00:03:26 -0400

    created a user page

--- app/routes.py
+++ app/routes.py
@@ -29,16 +29,15 @@ def login():
         return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.validate_on_submit():
-            user = User.query.filter_by(username=form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
-            return redirect(url_for("login"))
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get("next")
-        if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("index")
-        return redirect(url_for(next_page))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',  title='Sign In', form=form)
 
 @app.route("/signup")
@@ -70,3 +69,14 @@ def register():
         flash("Congrats, you are now a registered user")
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post 1"},
+        {"author": user, "body": "Test post 2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
