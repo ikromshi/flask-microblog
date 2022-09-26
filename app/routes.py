4059d072fd7bd6fc533b18d87920fdfba6180c9a
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-25 22:35:46 -0400

    set up the login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
-from app.forms import CreateAccountForm
+from flask_login import login_user, current_user
+from app.forms import CreateAccountForm, LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -23,10 +24,16 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
 
